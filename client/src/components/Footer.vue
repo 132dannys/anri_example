1ec726c961bab5d66eb82f9b509a34016b92ffe1
132dannys <Danlucky2003@gmail.com>
2023-08-14 14:41:59 +0300

    Add client

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+    <footer class="bg-dark text-center text-white">
+        <div class="container p-4 pb-0">
+            <section class="mb-4">
+                <!-- Instagram -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#"
+                    target="_blank" role="button"><i class="fab fa-instagram"></i></a>
+
+                <!-- Linkedin -->
+                <a class="btn btn-outline-light btn-floating m-1"
+                    href="#" target="_blank" role="button"><i
+                        class="fab fa-linkedin-in"></i></a>
+
+                <!-- Github -->
+                <a class="btn btn-outline-light btn-floating m-1"
+                    href="#" target="_blank" role="button"><i
+                        class="fab fa-github"></i></a>
+
+                <!-- Telegram -->
+                <a class="btn btn-outline-light btn-floating m-1" href="#" role="button"
+                    target="_blank"><i class="fab fa-telegram"></i></a>
+            </section>
+        </div>
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © 2023 :
+            <a class="text-white" href="/">Anri Application</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+</template>
+<script>
+
+</script>
+<style scoped>
+footer {
+    margin-top: auto;
+}
+</style>
