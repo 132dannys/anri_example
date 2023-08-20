c12b4470045d9aa21b259bb5d402292ff7917167
132dannys <Danlucky2003@gmail.com>
2023-08-20 11:57:37 +0300

    Add auth logic

--- api/src/main.py
+++ api/src/main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from adminsite import site
-
+from auth.base_config import fastapi_users, auth_backend
+from auth.schemas import UserRead, UserCreate
 
 app = FastAPI(
     title="Anri App"
 )
-
 
 origins = [
     "http://localhost:3000",
@@ -23,6 +23,18 @@ app.add_middleware(
 )
 
 site.mount_app(app)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["Auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["Auth"],
+)
 
 
 @app.get("/")
